6e5edbcea22b067edbc68487458cb030fa34cf08
Benjamin Taylor <tug42377@temple.edu>
2018-10-03 22:07:13 -0400

    Added Full Responsive Breakpoint Support, Reached base level API and ability to support responsive designs at component level, added CSS nano, removed xxl breakpoint to reduce size

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,13 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import sass from 'gulp-sass';
 import sassGlob from 'gulp-sass-glob';
+import cssnano from 'cssnano';
 
 
 gulp.task("sass", () => {
   return gulp.src('./src/**/*.scss')
   .pipe(sassGlob())
   .pipe(sass())
-  // .pipe(postcss([ autoprefixer() ]))
+  // .pipe(postcss([ cssnano() ]))
   .pipe(gulp.dest('./dist/css'))
 })
